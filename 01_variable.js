70eda1dc2151e8b66bb50d3dc6ec8cafda856e5f
parakhakshay <parakhakshay9@gmail.com>
2023-09-20 15:45:43 +0530

    new files has been added

---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+var firstName; // variable declaration
+firstName = "Akshay";  // Variable initialization
+console.log(firstName);
+firstName = "Mr. Akshay"; // Variable update
+console.log(firstName);
+
+
+var lastName = "Parakh"; // variable declaration and initialization
+console.log(lastName);
+
+var height; // Variable Declaration
+console.log(height);
+height = "6.1"; // initialization
+console.log(height);
+height = "6.5 feet";// update
+console.log(height);
---
+++ assignment/myFullName.js
@@ -0,0 +1,55 @@
+var myFullName;
+console.log(myFullName);
+
+var separator;
+separator = " =================####=================="; 
+console.log(separator);
+// or any other character you want to use as a separator between your first and last name
+myFullName = "Akshay Parakh";
+console.log(myFullName);
+
+
+var separator;
+separator = " =================####=================="; 
+console.log(separator);
+
+myFullName="Akshay Joesph Parakh"
+console.log(myFullName);
+
+var separator;
+separator = " =================####=================="; 
+console.log(separator);
+var myLove;
+myLove="I love only JS";
+console.log(myLove);
+
+var separator;
+separator = " =================####=================="; 
+console.log(separator);
+
+var firstName;
+firstName='Akshay';
+
+var lastName;
+lastName='Parakh';
+
+var age;
+age='24';
+
+var address;
+address='greenwood, USA'
+
+var pinCode;
+pinCode='105637';
+
+var gender;
+gender='male';
+
+var isMarried;
+isMarried='false';//true or false
+
+console.log(firstName,lastName,age,address,pinCode,gender,isMarried);
+
+
+
+
